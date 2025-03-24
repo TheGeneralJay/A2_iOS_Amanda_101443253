fd9ce046a4434dc61a031745aff312d1e529bada
Jay <amandagurns@hotmail.ca>
2025-03-24 12:57:52 -0400

    first commit

---
+++ A2_iOS_Amanda_101443253/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_iOS_Amanda_101443253
+//
+//  Created by Amanda Gurney on 2025-03-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
